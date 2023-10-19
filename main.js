885862e99efae19bbabb9fec91d43061c8ae500c
nicollialura <117859906+nicollialura@users.noreply.github.com>
2023-10-19 11:00:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-document.querySelector('.tecla_pom')
+alert('Essa página contém vírus');
+
+function tocaSomPom() {document.querySelector('#som_tecla_pom').play();}
+
+
+document.querySelector('.tecla_pom').onclick =tocaSomPom;
 document.querySelector('.tecla_clap')
 document.querySelector('.tecla_tim')
 document.querySelector('.tecla_puff')
@@ -8,4 +13,3 @@ document.querySelector('.tecla_psh')
 document.querySelector('.tecla_tic')
 document.querySelector('.tecla_tom')
 
-document.querySelector('#som_tecla_pom').play
